46bf489064cd0f084106642870aebdae86c5a7a7
Mikel Larreategi <mlarreategi@codesyntax.com>
2018-04-11 16:09:16 +0200

    basic tests

--- tests/test_pymipago.py
+++ tests/test_pymipago.py
@@ -3,10 +3,14 @@
 
 """Tests for `pymipago` package."""
 
-
+import datetime
+import os
 import unittest
 
-from pymipago import pymipago
+from pymipago import make_payment_request
+from pymipago.exceptions import InvalidCPRValue
+from pymipago.exceptions import InvalidFormatValue
+from pymipago.exceptions import InvalidReferenceNumber
 
 
 class TestPymipago(unittest.TestCase):
@@ -14,9 +18,87 @@ class TestPymipago(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        # set debug environment for testing
+        os.environ['DEBUG'] = 'True'
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
+        pass
 
-    def test_000_something(self):
-        """Test something."""
+    def test_invalid_cpr_raises_exception(self):
+        cpr = '010101'
+        sender = '123456'
+        format = '521'
+        suffix = '002'
+        reference_number = '1234567890'
+        payment_limit_date = datetime.datetime.now()
+        quantity = '1100'
+        language = 'eu'
+        return_url = 'http://localhost:8000'
+        payment_modes = ['01']
+
+
+        with self.assertRaises(InvalidCPRValue):
+            make_payment_request(
+                cpr,
+                sender,
+                format,
+                suffix,
+                reference_number,
+                payment_limit_date,
+                quantity,
+                language,
+                return_url,
+                payment_modes)
+
+
+    def test_invalid_format_raises_exception(self):
+        cpr = '9052180'
+        sender = '123456'
+        format = '855'
+        suffix = '002'
+        reference_number = '1234567890'
+        payment_limit_date = datetime.datetime.now()
+        quantity = '1100'
+        language = 'eu'
+        return_url = 'http://localhost:8000'
+        payment_modes = ['01']
+
+        with self.assertRaises(InvalidFormatValue):
+            make_payment_request(
+                cpr,
+                sender,
+                format,
+                suffix,
+                reference_number,
+                payment_limit_date,
+                quantity,
+                language,
+                return_url,
+                payment_modes)
+
+
+    def test_invalid_formated_reference_number_raises_exception(self):
+        cpr = '9052180'
+        sender = '123456'
+        format = '521'
+        suffix = '002'
+        reference_number = '12345678'
+        payment_limit_date = datetime.datetime.now()
+        quantity = '1100'
+        language = 'eu'
+        return_url = 'http://localhost:8000'
+        payment_modes = ['01']
+
+        with self.assertRaises(InvalidReferenceNumber):
+            make_payment_request(
+                cpr,
+                sender,
+                format,
+                suffix,
+                reference_number,
+                payment_limit_date,
+                quantity,
+                language,
+                return_url,
+                payment_modes)
