d85fde1487c95a979033e9f9a2aaf1babd1fe089
Mikel Larreategi <mlarreategi@codesyntax.com>
2020-01-21 16:55:22 +0100

    Back to development: 1.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/codesyntax/pymipago',
-    version='1.0',
+    version='1.1.dev0',
     zip_safe=False,
 )
